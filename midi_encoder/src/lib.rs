4b8df9a6c1e9a3717607e05527206bf4ba726445
Alan Liu <liualan998@gmail.com>
2024-12-21 13:57:33 -0500

    Simple MIDI sequence testing

--- midi_encoder/src/lib.rs
+++ midi_encoder/src/lib.rs
@@ -5,6 +5,8 @@ use midly::{Smf, Timing, TrackEventKind};
 
 
 const ENCODING_LENGTH: usize = 88 * 2;
+
+#[derive(Debug)]
 pub struct NoteEvent {
     timestamp: u32, // in ms
     key: u8,
@@ -14,6 +16,7 @@ pub struct NoteEvent {
 pub struct MIDIEncoding {
     encoding: Vec<Vec<f32>>,
 }
+
 
 impl NoteEvent {
     pub fn new(timestamp: u32, key: u8, note_on: bool) -> NoteEvent {
@@ -69,45 +72,51 @@ pub fn parse_midi(file_path: &str) -> Vec<NoteEvent> {
     events
 }
 
-pub fn encode_midi(events: Vec<NoteEvent>, timestep: &f32) -> MIDIEncoding {
-
+pub fn encode_midi(events: Vec<NoteEvent>, timestep_ms: u32) -> MIDIEncoding {
 
     let mut current_time: u32 = 0;
-    let timestep_ms: u32 = (*timestep * 1000.0) as u32;
 
     let mut encoding: Vec<Vec<f32>> = Vec::new();
-    let mut previous_note: Vec<f32> = vec![0.0; ENCODING_LENGTH];
+    let mut previous_note: Option<Vec<f32>> = None;
 
     for event in events {
-        if event.timestamp.abs_diff(current_time) < timestep_ms {
-            previous_note = layer(&previous_note, &get_note_encoding(event));
+        println!("Processing: {:?}", event);
+        if previous_note.is_some() && event.timestamp.abs_diff(current_time) < timestep_ms {
+            let prev: Vec<f32> = previous_note.expect("Cannot layer null note encoding");
+            let note: Vec<f32> = get_note_encoding(event).expect("Failed to get note encoding");
+            previous_note = layer(&prev, &note);
         } else {
-            encoding.push(previous_note.clone());
+            if previous_note.is_some() {
+                let note: Vec<f32> = previous_note.expect("Cannot get note encoding");
+                encoding.push(note);
+            }
             let next_time = event.timestamp;
             while current_time + timestep_ms <= next_time {
                 current_time += timestep_ms;
                 encoding.push(vec![0.0; ENCODING_LENGTH]);
+                println!("Current Time: {}", current_time);
             }
             previous_note = get_note_encoding(event);
+            current_time += timestep_ms;
         }
     }
-    encoding.push(previous_note);
+    encoding.push(previous_note.expect("Cannot get note encoding"));
 
-    MIDIEncoding { encoding: vec![] }
+    MIDIEncoding { encoding: encoding }
 }
 
 /* 
  MIDI values go from [21, 108]
  indices 2x = on/off, 2x + 1 for note ID 
 */
-fn get_note_encoding(note: NoteEvent) -> Vec<f32> {
+fn get_note_encoding(note: NoteEvent) -> Option<Vec<f32>> {
     let mut note_encoding: Vec<f32> = vec![0.0; ENCODING_LENGTH];
     let note_index: usize = (2 * (note.key - 21) + 1) as usize;
     note_encoding[note_index] = 1.0;
     if note.note_on {
         note_encoding[note_index - 1] = 1.0;
     }
-    note_encoding
+    Some(note_encoding)
 }
 
 fn decode_note_encoding(encoding: Vec<f32>, timestamp_ms: u32) -> Option<NoteEvent>{
@@ -126,7 +135,7 @@ fn decode_note_encoding(encoding: Vec<f32>, timestamp_ms: u32) -> Option<NoteEve
     Option::None
 }
 
-fn layer(n1: &Vec<f32>, n2: &Vec<f32>) -> Vec<f32> {
+fn layer(n1: &Vec<f32>, n2: &Vec<f32>) -> Option<Vec<f32>> {
     if n1.len() != n2.len() {
         panic!("Cannot add vectors of differing length");
     }
@@ -135,7 +144,7 @@ fn layer(n1: &Vec<f32>, n2: &Vec<f32>) -> Vec<f32> {
         new_encoding.push(n1[i] + n2[i]);
     }
 
-    new_encoding
+    Some(new_encoding)
 }
 
 #[cfg(test)]
@@ -144,7 +153,18 @@ mod tests {
 
     #[test]
     fn test_parse_midi() {
-        let events = parse_midi("path/to/midi/file.mid");
+        let events: Vec<NoteEvent> = parse_midi("./tests/Timing_Test.mid");
+        println!("{:?}", events);
         assert!(!events.is_empty());
+    }
+
+    #[test]
+    fn test_encode_midi() {
+        let events: Vec<NoteEvent> = parse_midi("./tests/Timing_Test.mid");
+        let encoding: MIDIEncoding = encode_midi(events, 500);
+        println!("{}", encoding.encoding.len());
+        for (i, vector) in encoding.encoding.iter().enumerate() {
+            println!("Timestep {i}: {:?}", vector);
+        }
     }
 }
